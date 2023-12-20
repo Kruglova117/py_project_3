e6243a99aaaa0f9abbd6075a445828ad9106f778
Pashkivska117 <kruglova_katerina>
2023-12-20 23:56:51 +0200

    done hm-2

---
+++ main.py
@@ -0,0 +1,9 @@
+start =int(input("Початок діапазону: "))
+end = int(input("Кінець діапазону: "))
+
+
+
+for i in range(start, end+1, ):
+    print(i)
+for i in range(start, end-1, 7,):
+    print(i)
