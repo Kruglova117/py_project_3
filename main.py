c25c950e4b71192b918bbef06fe26c15a45f8f22
Pashkivska117 <kruglova_katerina>
2023-12-22 08:58:33 +0200

    done hm-2

---
+++ main.py
@@ -0,0 +1,16 @@
+
+x1 = int(input("Ведіть перше число: "))
+x2 = int(input("Введіть друге число: "))
+x = x1 + 1
+sum += x
+x += 1
+while x < x2:
+    sum =int(input(x1 + x2))
+    x += 1
+    print("the sum of all integers between", x1, "and", x2, "is", sum)
+
+
+
+
+
+
