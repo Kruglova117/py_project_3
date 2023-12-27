01c520fd61b0320fc7bb2d23efb25701811522c2
Pashkivska117 <kruglova_katerina>
2023-12-27 19:34:26 +0200

    done hm ex-2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-start =int(input("Початок діапазону: "))
-end = int(input("Кінець діапазону: "))
+x1 =int(input("Початок діапазону: "))
+x2 = int(input("Кінець діапазону: "))
+X = x1 +1
+count = 0
 
-
-
-for i in range(start, end+1, ):
-    print(i)
-for i in range(start, end-1, 7,):
-    print(i)
+for i in range(x1, x2+1, 1):
+    if i % 5 ==0:
+        count += 1
+print(count, "Числа кратні 5" )
